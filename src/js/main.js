8e9fa860e82efd2d3913d541236515d3c2bf23b7
shermam.miranda <shermam.miranda@hotmail.com>
2017-02-13 02:23:14 -0200

    side-nav

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,15 @@
     'use strict'
 
     var menuButton = document.getElementById('menu-button');
+    var menuButtonClose = document.getElementById('menu-button-close');
     var nav = document.getElementById('nav');
 
     menuButton.addEventListener('click', function(){
         nav.classList.add('nav--visible');
+    });
+
+    menuButtonClose.addEventListener('click', function(){
+        alert('oi');
     });
 
 })()
